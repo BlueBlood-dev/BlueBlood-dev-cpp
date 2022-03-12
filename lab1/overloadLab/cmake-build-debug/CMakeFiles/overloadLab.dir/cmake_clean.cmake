88af980c0cb5e1ba55d4dfe33545ad02fb6610ee
&blueblood <khashchuk.denis@gmail.com>
2022-03-12 10:36:13 +0300

    lab 1 last init before pr

---
+++ lab1/overloadLab/cmake-build-debug/CMakeFiles/overloadLab.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/overloadLab.dir/main.cpp.o"
+  "liboverloadLab.dll.a"
+  "overloadLab.exe"
+  "overloadLab.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/overloadLab.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
